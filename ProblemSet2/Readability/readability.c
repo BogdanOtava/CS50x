15c0f7ddd14a9729c2cdf699032bea91988e555c
Bogdan Otava <bogdanotava097@gmail.com>
2022-11-03 18:29:05 +0200

    Added - ProblemSet2/Readability

---
+++ ProblemSet2/Readability/readability.c
@@ -0,0 +1,89 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+    // Get user input
+    string text = get_string("Text: ");
+
+    // Variables for letters, words, sentences
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    // Calculate average number of letters & average number of sentences
+    float avgLetters = (100 / (float)words) * (float)letters;
+    float avgSentences = (100 / (float)words) * (float)sentences;
+
+    // Calculate Coleman-Liau index
+    int index = round(0.0588 * avgLetters - 0.296 * avgSentences - 15.8);
+
+    // Print out index based on result
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
+
+// Determine the number of letters in a string
+int count_letters(string text)
+{
+    int letters = 0;
+
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    }
+
+    return letters;
+}
+
+// Determine the number of words in a string
+int count_words(string text)
+{
+    int words = 1;
+
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        if (text[i] == ' ')
+        {
+            words++;
+        }
+    }
+
+    return words;
+}
+
+// Determine the number of sentences in a string
+int count_sentences(string text)
+{
+    int sentences = 0;
+
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+
+    return sentences;
+}
