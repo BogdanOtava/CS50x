185c4d9324a12961f8477881ac85144ad542b12c
BogdanOtava <bogdanotava097@gmail.com>
2023-02-10 21:02:59 +0200

    Added - ProblemSet6/SentimentalMarioLess

---
+++ Week 6 - Python/ProblemSet6/SentimentalMarioLess/mario.py
@@ -0,0 +1,24 @@
+def get_height():
+    """Returns the height that was given by the user."""
+
+    while True:
+        try:
+            height = int(input("Height: "))
+
+            if height > 0 and height < 9:
+                return height
+
+        except ValueError as error:
+            print(error)
+
+
+def print_pyramid(height):
+    """Prints out the pyramid."""
+
+    for line in range(height):
+        print(" " * (height - line - 1), end="")
+        print("#" * (line + 1))
+
+
+# Call the function that prints the pyramid which takes as argument the function that returns the height
+print_pyramid(get_height())
